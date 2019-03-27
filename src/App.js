5d2bb3a5a99bd953de3a36b418aba1c6503b404e
kishore reddy <vvkishorereddy@gmail.com>
2019-03-27 16:15:37 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import "./App.css";
 import Router from "./Router/index";
+import { animateScroll as scroll } from "react-scroll";
 
 class App extends Component {
   render() {
-    return <Router />;
+    return (
+      <Fragment>
+        <Router />
+        <a onClick={() => scroll.scrollToTop()}>
+          <img
+            src="images/move-up.png"
+            alt=""
+            style={{
+              right: "20px",
+              marginRight: "30px",
+              marginBottom: "20px",
+              position: "absolute"
+            }}
+          />
+        </a>
+      </Fragment>
+    );
   }
 }
 
--- src/components/LeftSection.js
+++ src/components/LeftSection.js
@@ -17,57 +17,7 @@ export default class LeftSection extends Component {
             <input type="submit" defaultValue=" " />
           </form>
         </div>
-        {/* price range */}
-        <div className="range">
-          <h3 className="agileits-sear-head">Price range</h3>
-          <ul className="dropdown-menu6">
-            <li>
-              <div id="slider-range" />
-              <input
-                type="text"
-                id="amount"
-                style={{
-                  border: 0,
-                  color: "#ffffff",
-                  fontWeight: "normal"
-                }}
-              />
-            </li>
-          </ul>
-        </div>
-        {/* //price range */}
 
-        {/* discounts */}
-        <div className="left-side">
-          <h3 className="agileits-sear-head">Discount</h3>
-          <ul>
-            <li>
-              <input type="checkbox" className="checked" />
-              <span className="span">5% or More</span>
-            </li>
-            <li>
-              <input type="checkbox" className="checked" />
-              <span className="span">10% or More</span>
-            </li>
-            <li>
-              <input type="checkbox" className="checked" />
-              <span className="span">20% or More</span>
-            </li>
-            <li>
-              <input type="checkbox" className="checked" />
-              <span className="span">30% or More</span>
-            </li>
-            <li>
-              <input type="checkbox" className="checked" />
-              <span className="span">50% or More</span>
-            </li>
-            <li>
-              <input type="checkbox" className="checked" />
-              <span className="span">60% or More</span>
-            </li>
-          </ul>
-        </div>
-        {/* //discounts */}
         {/* reviews */}
         <div className="customer-rev left-side">
           <h3 className="agileits-sear-head">Customer Review</h3>
